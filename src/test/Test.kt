88a3674bd6c3b4a630d6b8b5e2e6ba4ac749b664
mrjctech <mrjctech@gmail.com>
2019-04-22 22:04:14 +0800

    fun

--- src/test/Test.kt
+++ src/test/Test.kt
@@ -4,9 +4,21 @@ package test
 fun main(args: Array<String>){
     println("-------------------------------")
     add(1,2)
+
+
+    //变量
+    var a = "hello"
+    a = "helloworld"
+    println(a)
+
+    //常量
+    val b = "nihao"
+//    b = "hello"
+    println(b)
+
 }
 
 
 fun add(a: Int, b: Int){
-    print(a + b)
+    println(a + b)
 }
