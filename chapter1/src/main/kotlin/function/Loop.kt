f7cb6c9bf5046ebdadbd8d46bed471b43a511fb0
mrjctech <mrjctech@gmail.com>
2019-08-11 17:04:29 +0800

    calc

--- chapter1/src/main/kotlin/function/Loop.kt
+++ chapter1/src/main/kotlin/function/Loop.kt
@@ -9,9 +9,17 @@ fun main() {
     for (a in charArrayOf){
         println(a)
     }
+    //a
+    //b
+    //c
+    //d
 
     for ((k, v) in charArrayOf.withIndex()){
         println("$k, $v")
     }
+    //0, a
+    //1, b
+    //2, c
+    //3, d
 
 }
